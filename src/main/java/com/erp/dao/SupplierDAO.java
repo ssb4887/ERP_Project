660e5555980cb0275e82b1e9e29ca09bcc7ef34c
ByeonSeongWK <92075727+ByeonSeongWK@users.noreply.github.com>
2022-01-07 16:55:01 +0900

    Update SupplierDAO.java

--- src/main/java/com/erp/dao/SupplierDAO.java
+++ src/main/java/com/erp/dao/SupplierDAO.java
@@ -9,5 +9,9 @@ public interface SupplierDAO {
 	
 	public List<Supplier> getSupplierList() throws Exception;
 	
+	public List<Supplier> searchSupplier(String supp_name) throws Exception;
+	
 	public void addSupplier(Supplier supplier) throws Exception;
+	
+	public List<Supplier> searchSupplier_Del(String supp_id) throws Exception;
 }
