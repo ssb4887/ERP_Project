48e5fc4124d3c04c4db0d3af79b2230e0e04fa18
ByeonSeongWK <92075727+ByeonSeongWK@users.noreply.github.com>
2022-01-07 16:55:51 +0900

    Update SupplierService.java

--- src/main/java/com/erp/service/SupplierService.java
+++ src/main/java/com/erp/service/SupplierService.java
@@ -9,5 +9,9 @@ public interface SupplierService {
 	
 	public List<Supplier> getSupplierList() throws Exception;
 	
+	public List<Supplier> searchSupplier(String supp_name) throws Exception;
+	
 	public void addSupplierAction(Supplier supplier) throws Exception;
+	
+	public List<Supplier> searchSupplier_Del(String supp_id) throws Exception;
 }
